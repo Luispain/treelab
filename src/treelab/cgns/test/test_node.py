fixed resizing operation when saving individual node numerical value in hdf5 since it does not use chunks

src/treelab/cgns/test/test_node.py
@@ -290,5 +290,18 @@ def test_load_workflow_parameters():
     os.unlink('test.cgns')
     assert str(wf) == str(wf_init)
 
+def test_saveThisNodeOnly():
+    t = cgns.Tree()
+    n = cgns.Node(Name='testNode',Value=np.array([0]))
+    n.attachTo(t)
+    t.save('test.cgns',backend='h5py2cgns')
+    n_value = n.value()
+    n_value += 1
+    n.saveThisNodeOnly('test.cgns',backend='h5py2cgns')
+    t_updated = cgns.load('test.cgns')
+    os.unlink('test.cgns')
+    n_value_updated = t_updated.get('testNode').value()
+    assert n_value[0] == n_value_updated[0]
+
 if __name__ == '__main__':
-    test_load_workflow_parameters()
+    test_saveThisNodeOnly()