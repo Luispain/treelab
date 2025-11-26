112c1daf23854bc1284c56a522e78cd3b8649074
Luis Bernardos <luis.bernardos@onera.fr>
2025-11-26 10:00:32 +0100

    new version 0.4.4

--- src/treelab/__init__.py
+++ src/treelab/__init__.py
@@ -15,7 +15,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with MOLA.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.4.3'
+__version__ = '0.4.4'
 
 try:
     import os
